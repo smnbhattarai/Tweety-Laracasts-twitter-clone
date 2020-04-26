2314291aff7c46abb83b6b4dce426c706ff4101c
suman <suman.bhattarai@gmail.com>
2020-04-26 23:56:37 +0545

    Design the Timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,13 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" class="rounded-full mr-2">
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad, aliquid architecto, aspernatur beatae dicta, doloribus esse exercitationem illo ipsum laboriosam nesciunt quia quisquam totam ut velit veritatis vitae voluptas.
+        </p>
+    </div>
+</div>
