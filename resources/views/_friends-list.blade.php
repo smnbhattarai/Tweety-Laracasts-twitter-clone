cfa333f27b0d88c4d053461260df82358c3314c8
suman <suman.bhattarai@gmail.com>
2020-04-27 22:11:15 +0545

    Build a Following system

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,11 +1,11 @@
 <h3 class="font-bold text-xl mb-4">Friends</h3>
 
 <ul>
-    @foreach(range(1, 8) as $index)
+    @foreach(auth()->user()->follows as $user)
     <li class="mb-4">
         <div class="flex items-center text-sm">
-            <img src="https://i.pravatar.cc/40" alt="" class="rounded-full mr-2">
-            John Doe
+            <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="rounded-full mr-2">
+            {{ $user->name }}
         </div>
     </li>
         @endforeach
